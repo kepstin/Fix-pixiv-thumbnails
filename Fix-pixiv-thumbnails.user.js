4e1f1b188f74feadd33420ced64dd945b09accdf
Calvin Walton <calvin.walton@kepstin.ca>
2020-01-26 14:27:17 -0500

    Add support for new search page (/tags/* and /en/tags/*)

--- Fix-pixiv-thumbnails.user.js
+++ Fix-pixiv-thumbnails.user.js
@@ -3,7 +3,7 @@
 // @name:ja        pixivサムネイルを改善する
 // @namespace      https://www.kepstin.ca/userscript/
 // @license        MIT; https://spdx.org/licenses/MIT.html
-// @version        20190925.0
+// @version        20191120.0
 // @description    Stop pixiv from cropping thumbnails to a square. Use higher resolution thumbnails on Retina displays.
 // @description:ja 正方形にトリミングされて表示されるのを防止します。Retinaディスプレイで高解像度のサムネイルを使用します。
 // @author         Calvin Walton
@@ -16,6 +16,7 @@
 // @match          https://www.pixiv.net/discovery/users
 // @match          https://www.pixiv.net/en/
 // @match          https://www.pixiv.net/en/artworks/*
+// @match          https://www.pixiv.net/en/tags/*
 // @match          https://www.pixiv.net/history.php*
 // @match          https://www.pixiv.net/howto*
 // @match          https://www.pixiv.net/member.php*
@@ -25,6 +26,7 @@
 // @match          https://www.pixiv.net/ranking.php*
 // @match          https://www.pixiv.net/search.php*
 // @match          https://www.pixiv.net/stacc*
+// @match          https://www.pixiv.net/tags/*
 // @grant          none
 // ==/UserScript==
 
