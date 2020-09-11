da81c3a6064e8bac5c388d138eee435b3315692c
Calvin Walton <calvin.walton@kepstin.ca>
2020-09-10 22:00:49 -0400

    Avoid accessing computed size on div img replacement
    Doing so makes it wait for the image to load. With this change, the
    browser can cancel the original image load. This significantly speeds up
    page loads on some browsing pages.

--- Fix-pixiv-thumbnails.user.js
+++ Fix-pixiv-thumbnails.user.js
@@ -3,7 +3,7 @@
 // @name:ja        pixivサムネイルを改善する
 // @namespace      https://www.kepstin.ca/userscript/
 // @license        MIT; https://spdx.org/licenses/MIT.html
-// @version        20200523.1
+// @version        20200910.1
 // @updateURL      https://raw.githubusercontent.com/kepstin/Fix-pixiv-thumbnails/master/Fix-pixiv-thumbnails.user.js
 // @description    Stop pixiv from cropping thumbnails to a square. Use higher resolution thumbnails on Retina displays.
 // @description:ja 正方形にトリミングされて表示されるのを防止します。Retinaディスプレイで高解像度のサムネイルを使用します。
@@ -194,7 +194,10 @@
         if (!m) { node.dataset.kepstinThumbnail = 'bad'; return; }
         if (node.dataset.kepstinThumbnail == m.path) { return; }
 
-        let size = Math.max(node.clientWidth, node.clientHeight);
+        let size = Math.max(
+            Number(node.style.width.replace(/[^\d\.\-]/g, '')),
+            Number(node.style.height.replace(/[^\d\.\-]/g, ''))
+        );
         if (size == 0) { size = Math.max(m.width, m.height); }
 
         if (node.firstElementChild) {
