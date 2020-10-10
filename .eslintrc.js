45d7f673724ceba3363c13f52a8ad070ddb457bf
Calvin Walton <calvin.walton@kepstin.ca>
2020-10-10 19:00:26 -0400

    eslint: Handle g*monkey globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    es2017: true
+    es2017: true,
+    greasemonkey: true
   },
   extends: [
     'standard'
   ],
   rules: {
     'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
-    'max-len': ['error', { code: 128 }]
+    'max-len': ['error', { code: 128 }],
+    camelcase: ['error', { allow: ['^GM_'] }]
   }
 }
